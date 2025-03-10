9fb41a7789b454939afe3150f8e5ccd07fbfc685
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-10 10:09:58 +0500

    Complete toggle project

--- React/frontend/src/projects/toggle/Toggle.jsx
+++ React/frontend/src/projects/toggle/Toggle.jsx
@@ -3,11 +3,24 @@ import { useState } from "react";
 const Toggle = () => {
   const [toggle, setToggle] = useState(false);
 
+  const clickToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <>
       <div className="h-dvh w-full bg-indigo-900 flex flex-col items-center justify-center">
-        <button className="h-20 w-60 p-2 bg-gray-900 rounded-full cursor-pointer">
-          <div className="h-15 w-15 bg-blue-800 rounded-full"></div>
+        <button
+          onClick={clickToggle}
+          className={`h-20 w-50 p-2 rounded-full cursor-pointer ${
+            toggle ? "bg-green-500" : "bg-gray-300"
+          }`}
+        >
+          <div
+            className={`h-15 w-15 bg-gray-200 rounded-full transition-transform duration-500 ${
+              toggle ? "translate-x-31" : "translate-x-0"
+            }`}
+          ></div>
         </button>
       </div>
     </>
