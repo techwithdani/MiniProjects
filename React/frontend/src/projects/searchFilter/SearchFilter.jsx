cf5ad531c11c7bcb5688175522451d2e4c8274b6
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-16 14:36:14 +0500

    Create search filter

--- React/frontend/src/projects/searchFilter/SearchFilter.jsx
+++ React/frontend/src/projects/searchFilter/SearchFilter.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const SearchFilter = () => {
+  const [items] = useState([
+    "Football",
+    "Basketball",
+    "Hockey",
+    "Squash",
+    "Athletics",
+    "Cricket",
+    "Volleyball",
+    "Boxing",
+    "Callisthenics",
+  ]);
+  const [search, setSearch] = useState("");
+
+  const filteredItems = items.filter((item) =>
+    item.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
-      <h1>Search Filter</h1>
+      <div className="w-full h-dvh bg-gray-800 flex justify-center items-center">
+        <div>
+          <div>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <div>
+            <ul>
+              {filteredItems.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
